8a1b27e629acbf99847356b9fc96591bcd5f47e9
raushan <kraushan022@gmail.com>
2025-04-17 11:33:37 +0530

    header and body

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY="
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const ResturantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvxp5xbniat84r6efku2"
+        alt="res-logo"
+      />
+      <h3>Home Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResturantCard />
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
