40f4bbd03eb62aee501462f54f82e7ea13cc96c5
raushan <kraushan022@gmail.com>
2025-04-19 16:07:28 +0530

    added filter functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { resList } from "../utils/mockData";
 import ResturantCard from "./ResturantCard";
 
 const Body = () => {
+  //Local State Variable - super powerful variable
+  const [listOfResturant, setListOfResturant] = useState(resList);
+
+  const handleFilter = () => {
+    const filterdResturant = listOfResturant.filter((res) => {
+      return res.info.avgRating >= 4.3;
+    });
+    setListOfResturant(filterdResturant);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={handleFilter}>
+          Filter Resturant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((resturant) => (
+        {listOfResturant.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
