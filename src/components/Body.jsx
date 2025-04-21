7edd4f421729193d3301d3df9b353789d358d9d4
raushan <kraushan022@gmail.com>
2025-04-21 11:38:54 +0530

    live data api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,34 @@
-import { useState } from "react";
-import { resList } from "../utils/mockData";
+import { useEffect, useState } from "react";
 import ResturantCard from "./ResturantCard";
 
 const Body = () => {
   //Local State Variable - super powerful variable
-  const [listOfResturant, setListOfResturant] = useState(resList);
+  const [listOfResturant, setListOfResturant] = useState([]);
 
   const handleFilter = () => {
     const filterdResturant = listOfResturant.filter((res) => {
-      return res.info.avgRating >= 4.3;
+      return res.info.avgRating >= 4.4;
     });
     setListOfResturant(filterdResturant);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1286243&lng=91.8012675&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    console.log(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setListOfResturant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
   return (
