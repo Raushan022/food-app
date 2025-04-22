fe71e128a8efcbfeb7f3e4bdb843c8c82554e7fe
raushan <kraushan022@gmail.com>
2025-04-23 00:23:31 +0530

    children routes in react-router-dom

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "./Header";
 
 const Contact = () => {
   return (
     <div>
-      <Header />
       <h1>Contact us</h1>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Error from "./components/Error.jsx";
+import Body from "./components/Body.jsx";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
     errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
   },
 ]);
 
