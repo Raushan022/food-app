6a3261dba6744191ce4505a9b0f0fdf693930243
raushan <kraushan022@gmail.com>
2025-04-25 11:02:25 +0530

    class based component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is my Food ordering app</h2>
-    </div>
-  );
-};
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is my Food ordering app</h2>
+        {/* <User name={"Raushan kumar - function"} /> */}
+        <UserClass name={"Raushan kumar - class"} location="Bihar class" />
+      </div>
+    );
+  }
+}
 
 export default About;
---
+++ src/components/UserClass.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInfo: {
+        login: "Dummy",
+        html_url: "location",
+      },
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/Raushan022");
+    const json = await data.json();
+
+    console.log(json);
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  render() {
+    const { login, html_url } = this.state.userInfo;
+    return (
+      <div className="class-user-card">
+        <h4>Login: {login}</h4>
+        <h3>html url: {html_url}</h3>
+        <h4>Contact: @raushan002</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
+
+/*
+--- MOUNTING---
+   constructor
+   render(dummy)
+      <HTML Dummy>
+   component Did Mount
+      <API call>
+      <this.setState -> state variable updates
+
+----UPDATE---
+
+   render(API data)
+   <HTML (new api data)>
+   Component Did Update
+*/
